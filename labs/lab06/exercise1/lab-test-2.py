a74a02048a670c7b9c2d293a0914c3cefb2e63c9
sabr1n-hub <sabrinzss69@gmail.com>
2026-08-11 11:28:55 +0800

    finish lab 6

---
+++ labs/lab06/exercise1/lab-test-2.py
@@ -0,0 +1,21 @@
+#Enter Programmers Information
+Programmers_Name="Ahmad Bin Abu"
+Matric_No="MS2025123499"
+Total_marks=2*10     #artihmetic expression
+print( 
+  f"Name:{Programmers_Name}\t\t\tMatric.\tNo:{Matric_No}\n", # header
+  f"*\t\t\t*\n",
+  f"**\t\t**\n",
+  f"***\t***\n",
+  f"********\n",
+  f"***\t***\n",
+  f"**\t\t**\n",
+  f"*\t\t\t*\n\n",
+  f"This is my\n",
+  f"\tsecond\n",
+  f"\t\tassignment\n",
+  f"I want 2x10 marks,which is {Total_marks} full marks"
+
+      )
+
+ 
